d5f1e17b642809b02edb9687d19a922d0a271c9c
M. Shafiq <mshafiq9@gmail.com>
2022-02-02 12:48:06 +0400

    sorting imports and adding comments

--- hello.go
+++ hello.go
@@ -1,18 +1,22 @@
 package main
 
 import "fmt"
-
-import "rsc.io/quote"
-import "github.com/gorilla/mux"
 import "net/http"
 
+import "github.com/gorilla/mux"
+import "rsc.io/quote"
+
+
 func main() {
+    // printing on console
     fmt.Println("Hello, World!")
     fmt.Println(quote.Go())
 
+    // create a mux router and register the home page func
     r := mux.NewRouter()
     r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "Hello")})
 
+    // run server on 10001 port
     http.Handle("/", r)
     http.ListenAndServe(":10001", nil)
 }
